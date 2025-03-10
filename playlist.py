bb2048d42ea14ce605e878b627cc22d5950b825f
Vadaliya Falsi <110164717+Falsi3007@users.noreply.github.com>
2025-03-10 18:56:36 +0530

    Update playlist.py

--- playlist.py
+++ playlist.py
@@ -1,28 +1,25 @@
-from googleapiclient.discovery import build
+import requests
 import pandas as pd
- 
+
 api_key = 'AIzaSyC41Mb1yBOBA6qX8ahORhWNhMkInIGjCgE'
- 
 channel_ids = ['UCsvqVGtbbyHaMoevxPAq9Fg', 'UCnfZSN7A09wNwYiUoincXZg']
- 
-api_service_name = "youtube"
-api_version = "v3"
- 
-youtube = build(api_service_name, api_version, developerKey=api_key)
- 
-def get_channel_playlists(youtube, channel_ids):
+
+def get_channel_playlists(api_key, channel_ids):
     all_data = []
-   
+    base_url = 'https://www.googleapis.com/youtube/v3/playlists'
+    
     for channel_id in channel_ids:
         next_page_token = None
-       
+        
         while True:
-            response = youtube.playlists().list(
-                part="snippet,contentDetails",
-                channelId=channel_id,
-                pageToken=next_page_token
-            ).execute()
-           
+            params = {
+                'part': 'snippet,contentDetails',
+                'channelId': channel_id,
+                'key': api_key,
+                'pageToken': next_page_token
+            }
+            response = requests.get(base_url, params=params).json()
+            
             for item in response.get('items', []):
                 data = {
                     'ChannelId': channel_id,
@@ -31,13 +28,14 @@ def get_channel_playlists(youtube, channel_ids):
                     'TotalVideos': item['contentDetails']['itemCount']
                 }
                 all_data.append(data)
-           
+            
             next_page_token = response.get('nextPageToken')
             if not next_page_token:
                 break
-   
+    
     pd.set_option('display.max_columns', None)
     df = pd.DataFrame(all_data)
     print(df)
-   
-get_channel_playlists(youtube, channel_ids)
+
+# Call the function to get output
+get_channel_playlists(api_key, channel_ids)
